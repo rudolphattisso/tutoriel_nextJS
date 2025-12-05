6ec84a99cc62c89b312d4cb1a7cdd395d42b994b
Rudolph ATTISSO <rudolph.a@gallusia.com>
2025-12-05 13:37:11 +0100

    first commit

---
+++ app/admin/page.tsx
@@ -0,0 +1,11 @@
+import { Card, CardHeader, CardTitle } from "@/src/components/ui/card";
+
+export default function Page(){
+    return(
+        <Card>
+            <CardHeader>
+                <CardTitle>URL : /admin</CardTitle>
+            </CardHeader>
+        </Card>
+    );
+}
---
+++ src/components/ui/header.tsx
@@ -0,0 +1,9 @@
+import { Card } from "@/src/components/ui/card";
+
+export function Header() {
+  return (
+    <Card className="p-4">
+        <p>CitationMaster (layout)</p>
+    </Card>
+  );
+}
